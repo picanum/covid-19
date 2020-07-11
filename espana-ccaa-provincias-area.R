eeaa42f752ba11bb6187deb0b593fe5418e81488
picanum <10380748+picanum@users.noreply.github.com>
2020-07-11 13:44:36 +0200

    Update espana-ccaa-provincias-area.R

--- espana-ccaa-provincias-area.R
+++ espana-ccaa-provincias-area.R
@@ -27,14 +27,14 @@ datos %>% select(ccaa_iso, fecha, num_casos_prueba_pcr) %>%
   annotate("text", x = as.Date("2020-04-01"), y = 46.5, label = "Galicia", size = 4, angle = -25) +
   labs(x = "Fecha", y = "Porcentaje de casos",
        title = "¿De qué Comunidades Autónomas proceden los nuevos casos diarios de COVID-19? ¿En qué porcentaje lo hacen?",
-       subtitle = "Porcentaje que cada CA representa dentro de la media móvil (ventana 7 días) de nuevos casos diarios en España. Fuente: https://cnecovid.isciii.es/covid19/",
+       subtitle = "Porcentaje que cada CA representa dentro de la media móvil (ventana 7 días) de nuevos casos diarios por PCR+ en España. Fuente: https://cnecovid.isciii.es/covid19/",
        caption = "Actualizado a 11/07/2020 | @Picanumeros") +
   scale_x_date(breaks = "14 days", date_labels = "%d %b", expand = c(0,0)) +
   scale_y_continuous(breaks = seq(0, 100, by = 10), expand = c(0,0)) +
   theme_minimal(base_size = 14) + 
   theme(panel.grid = element_line(colour = "black"),
         text = element_text(family = "Liberation Sans"))
-ggsave("desglose_ccaa.png", dpi = 300, width = 14.8, height = 8)
+ggsave("desglose_ccaa.png", dpi = 300, width = 15.2, height = 8)
 
 datos <- readr::read_csv("https://cnecovid.isciii.es/covid19/resources/datos_provincias.csv")
 
@@ -49,7 +49,7 @@ datos %>% select(provincia_iso, fecha, num_casos_prueba_pcr) %>%
   geom_area(col = "black", alpha = .925) + scale_fill_manual(name = "Provincia\n(código ISO)", values = getPalette(52)) +
   labs(x = "Fecha", y = "Porcentaje de casos",
        title = "¿De qué provincias proceden los nuevos casos diarios de COVID-19? ¿En qué porcentaje lo hacen?",
-       subtitle = "Porcentaje que cada provincia representa dentro de la media móvil de nuevos casos diarios en España. Fuente: https://cnecovid.isciii.es/covid19/",
+       subtitle = "Porcentaje que cada provincia representa dentro de la media móvil (ventana de 7 días) de nuevos casos diarios por PCR+ en España. Fuente: https://cnecovid.isciii.es/covid19/",
        caption = "Actualizado a 11/07/2020 | @Picanumeros") +
   scale_x_date(breaks = "14 days", date_labels = "%d %b", expand = c(0,0)) +
   scale_y_continuous(breaks = seq(0, 100, by = 10), expand = c(0,0)) +
@@ -61,4 +61,4 @@ datos %>% select(provincia_iso, fecha, num_casos_prueba_pcr) %>%
   annotate("text", x = as.Date("2020-04-30"), y = 87, label = "Barcelona", size = 7) +
   annotate("text", x = as.Date("2020-06-21"), y = 62.5, label = "Huesca", size = 5, angle = 30) +
   annotate("text", x = as.Date("2020-06-21"), y = 21.75, label = "Málaga", size = 3)
-ggsave("desglose_prov.png", dpi = 300, width = 14.8, height = 8)
+ggsave("desglose_prov.png", dpi = 300, width = 15.8, height = 8)
